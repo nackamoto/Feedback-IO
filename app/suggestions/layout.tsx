a059bcfbc9b8b86569f307a290ac3e274e767312
defscarr <anon.aryan.admin@protonmail.com>
2023-06-27 07:56:50 +0000

    update(suggestions): add content to suggestion page
    populated the suggestions page with data from 'data.json'

--- app/suggestions/layout.tsx
+++ app/suggestions/layout.tsx
@@ -1,7 +1,7 @@
 import { Metadata } from "next";
 
 export const metadata:Metadata = {
-    title: 'suggestions',
+    title: 'Suggestions',
     description: ''
 }
 interface IProps {
--- app/suggestions/page.tsx
+++ app/suggestions/page.tsx
@@ -1,8 +1,8 @@
 'use client'
-import { CardTabletPlus } from "@/components/suggestions/card-tabletplus";
-import { CardMobile } from "@/components/suggestions/card-mobile";
+import { SuggestionsTablet } from "@/components/suggestions/suggestions-tablet";
+import { SuggestionsMobile } from "@/components/suggestions/suggestions-mobile";
 import { useProps } from "@/context/app-theme";
-import { sugProps } from "@/components/suggestions/card-tabletplus";
+import { sugProps } from "@/components/suggestions/suggestions-tablet";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
@@ -19,32 +19,37 @@ export default function SuggestionPage(){
 
   
   const [filterBy, setFilterBy] = useState<string>('all');
-  const [sortBy, setSortBy] = useState("more");
+  const [sortBy, setSortBy] = useState<string>("more");
 
 
 
   const [category, setCategory] = useState
-  <{
-    all:boolean,
-    ui:boolean, 
-    ux:boolean, 
-    feature:boolean, 
-    enhancement:boolean, 
-    bug: boolean
-  }>
-    ({
-      
-      all:true,
-      ui:false,
-      ux:false,
-      feature:false,
-      enhancement:false,
-      bug:false
-    });
-                                              
-  const suggestions = filterBy === 'all' ? datastore: datastore.filter(card => card.category === filterBy);
-                                              
-      
+              <{
+                all:boolean,
+                ui:boolean, 
+                ux:boolean, 
+                feature:boolean, 
+                enhancement:boolean, 
+                bug: boolean
+              }>
+                ({
+                  
+                  all:true,
+                  ui:false,
+                  ux:false,
+                  feature:false,
+                  enhancement:false,
+                  bug:false
+                });
+  
+  const [Suggestions, setSuggestions] = useState(() => datastore.filter(item => item.status === 'suggestion'))                                        
+
+  useEffect(() => {
+
+    setSuggestions(filterBy === 'all' ? datastore.filter(item => item.status === 'suggestion') : datastore.filter(item => item.status === 'suggestion' && item.category === filterBy))
+  }, [filterBy, datastore]);
+
+
 
   useEffect(() => {
         
@@ -63,28 +68,22 @@ export default function SuggestionPage(){
     }, [filterBy]);
 
 
-  useEffect(() => {
-    // logic to sort "setDatastore"
-    
-  }, [sortBy]);
-
-
 
 
   const DesktopSuggestion = () => {
 
 
-    const cards = suggestions.map((card:sugProps, i:number) => (
+    const items = Suggestions.map((item:sugProps, i:number) => (
       
-        <CardTabletPlus key={i} 
-            id={card.id}
-            upvotes={card.upvotes} 
-            title={card.title} 
-            description={card.description} 
-            category={card.category} 
-            comment={card.comments ? card.comments.length : 0}
-            status={card.status}
-            isToggled={card.isToggled}
+        <SuggestionsTablet key={i} 
+            id={item.id}
+            upvotes={item.upvotes} 
+            title={item.title} 
+            description={item.description} 
+            category={item.category} 
+            comment={item.comments ? item.comments.length : 0}
+            status={item.status}
+            isToggled={item.isToggled}
 
 
         />
@@ -92,29 +91,29 @@ export default function SuggestionPage(){
 
 
 
-      return cards;
+      return items;
 
 
   };
 
   const MobileSuggestions = () => {
 
-    const cards:JSX.Element = datastore.map((card:sugProps, i:number) => (
+    const items:JSX.Element = Suggestions.map((item:sugProps, i:number) => (
 
-      <CardMobile key={i} 
-          id={card.id}
-          upvotes={card.upvotes} 
-          title={card.title} 
-          description={card.description} 
-          category={card.category} 
-          comment={card.comments ? card.comments.length : 0}
-          status={card.status}
-          isToggled={card.isToggled}
+      <SuggestionsMobile key={i} 
+          id={item.id}
+          upvotes={item.upvotes} 
+          title={item.title} 
+          description={item.description} 
+          category={item.category} 
+          comment={item.comments ? item.comments.length : 0}
+          status={item.status}
+          isToggled={item.isToggled}
 
       />
   ));
 
-    return cards;
+    return items;
 
 
 };
@@ -218,7 +217,7 @@ export default function SuggestionPage(){
               <span>
                 <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg"><path d="M11.5 2.274c2.237 0 4.339.854 5.923 2.408a8.123 8.123 0 012.465 5.839 8.084 8.084 0 01-1.7 4.979 8.457 8.457 0 01-3.652 2.71l-.31.112.003.826h.369c.262 0 .475.21.475.469a.47.47 0 01-.39.46l-.085.008h-.365l.004 1.02h.36c.263 0 .476.21.476.469a.47.47 0 01-.39.461l-.085.008h-.358l.006 1.487a.466.466 0 01-.381.46l-.094.01H9.23a.478.478 0 01-.466-.378l-.01-.092.006-1.487h-.357a.472.472 0 01-.475-.47.47.47 0 01.39-.46l.085-.008h.361l.004-1.02h-.365a.472.472 0 01-.475-.468.47.47 0 01.39-.462l.085-.007h.368l.004-.826a8.452 8.452 0 01-3.996-2.867 8.08 8.08 0 01-1.666-5.056c.032-2.127.923-4.152 2.511-5.7 1.508-1.471 3.448-2.322 5.493-2.416l.324-.009h.06zm1.791 19.769H9.709l-.004 1.02h3.59l-.004-1.02zm-.007-1.958H9.716l-.003 1.02h3.574l-.003-1.02zM11.5 3.212h-.054c-3.946.027-7.327 3.325-7.384 7.2-.048 3.266 2.14 6.192 5.322 7.118.174.05.3.193.332.364l.008.088-.004 1.166h3.56l-.004-1.166a.47.47 0 01.34-.452c3.134-.912 5.323-3.794 5.323-7.01a7.197 7.197 0 00-2.185-5.173A7.453 7.453 0 0011.5 3.212zm.829 1.782a.4.4 0 01.401.397v.322c.48.12.932.307 1.346.552l.228-.226a.405.405 0 01.569 0L16.046 7.2a.393.393 0 010 .56l-.23.228c.247.41.437.858.557 1.333h.323a.4.4 0 01.402.397v1.645a.4.4 0 01-.402.396h-.323c-.12.476-.31.924-.557 1.333l.23.228a.393.393 0 010 .56l-1.173 1.163a.405.405 0 01-.57 0l-.227-.227a5.02 5.02 0 01-1.346.553v.322a.4.4 0 01-.401.396H10.67a.4.4 0 01-.402-.396v-.322a5.022 5.022 0 01-1.345-.553l-.228.227a.405.405 0 01-.569 0L6.954 13.88a.393.393 0 010-.56l.23-.228a4.924 4.924 0 01-.557-1.333h-.324a.4.4 0 01-.401-.396V9.719a.4.4 0 01.401-.397h.324c.12-.475.31-.923.557-1.333l-.23-.228a.393.393 0 010-.56L8.127 6.04a.405.405 0 01.569 0l.228.226a5.021 5.021 0 011.345-.552V5.39a.4.4 0 01.402-.397zM11.5 7.721c-1.572 0-2.846 1.263-2.846 2.82 0 1.558 1.274 2.82 2.846 2.82s2.846-1.262 2.846-2.82c0-1.557-1.274-2.82-2.846-2.82zm11.025 4.152c.262 0 .475.21.475.469a.47.47 0 01-.39.461l-.085.008h-.498a.472.472 0 01-.475-.469.47.47 0 01.39-.461l.085-.008h.498zm-21.552 0c.262 0 .475.21.475.469a.47.47 0 01-.39.461l-.085.008H.475A.472.472 0 010 12.342a.47.47 0 01.39-.461l.085-.008h.498zM3.112 3.45l.074.06.46.451c.185.183.186.48 0 .663a.476.476 0 01-.596.062l-.075-.06-.459-.451a.465.465 0 01-.001-.663.48.48 0 01.597-.062zm17.373.062c.162.16.182.408.06.59l-.061.073-.46.45a.476.476 0 01-.67 0 .464.464 0 01-.06-.59l.06-.074.46-.45a.48.48 0 01.671 0zM11.5 0c.233 0 .427.166.467.384l.008.085v.49a.472.472 0 01-.475.468.473.473 0 01-.467-.384l-.008-.084v-.49c0-.26.213-.469.475-.469z" fill="#FFF" /></svg>
               </span>
-              <h3 className="font-bold text-18x leading-26 tracking-closer">{suggestions.length}&nbsp;Suggestions</h3>
+              <h3 className="font-bold text-18x leading-26 tracking-closer">{Suggestions.length}&nbsp;Suggestions</h3>
             </div>
             <div className="flex-1 flex text-xSiolet-50 items-center">
               <span className="font-normal text-13x tablet:text-14x hover:cursor-pointer ">Sort by&nbsp;:&nbsp;</span>
@@ -237,12 +236,12 @@ export default function SuggestionPage(){
           
 
           
-          <div className={`hidden tablet:flex flex-col ${suggestions.length < 1 ? "space-y-0 overflow-scroll": "space-y-4 overflow-y-auto"}`}>
-            {suggestions.length < 1 ? <Feedback404/>  : DesktopSuggestion() }
+          <div className={`hidden tablet:flex flex-col ${Suggestions.length < 1 ? "space-y-0 overflow-scroll": "space-y-4 overflow-y-auto"}`}>
+            {Suggestions.length < 1 ? <Feedback404/>  : DesktopSuggestion() }
           </div>
 
           <div className="flex flex-col px-6 pt-8 pb-14 space-y-4 overflow-y-auto tablet:hidden">
-            {MobileSuggestions()}
+            {Suggestions.length < 1 ? <Feedback404/>  : MobileSuggestions() }
           </div>
 
         </section>

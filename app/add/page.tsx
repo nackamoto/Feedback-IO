d50ff25c2e3d452525c148720373bc2cb46ef207
Nvic1 <denfu.oscar.dev@gmail.com>
2023-07-01 17:21:54 +0000

    refactor: replace html "select" element to use custom dropdowns
    Default html selectors have been replace with custom dropdown selectors

--- app/add/page.tsx
+++ app/add/page.tsx
@@ -2,22 +2,29 @@
 import { useRouter } from "next/navigation";
 import { useProps } from "@/context/app-theme";
 import { SubmitHandler, useForm } from "react-hook-form";
+import { useEffect, useRef, useState } from "react";
+
+
 
 
 interface Input{
     title: string,
-    category: string,
     description:string
 }
 
 export default function NewFeedback(){
 
+
+
+
     const router = useRouter()
-    const {datastore,setDatastore} = useProps();
+    const {datastore, setDatastore} = useProps();
+
+    const [SelectedCategory, setSelectedCategory] = useState<string>('Feature');
+    const [Category, setCategory] = useState<string>('feature');
 
     const {register, handleSubmit, formState:{errors}} = useForm<Input>({defaultValues: {
         title: '',
-        category: 'feature',
         description:'',
     }});
 
@@ -30,13 +37,17 @@ export default function NewFeedback(){
                 isToggled: false,
                 comments: [],
                 ...data, 
+                category: Category
             }])
 
         console.log(`add new product request with title: \'${data.title}\'`)
         
     }
 
-    console.log(datastore)
+    const useCategory = (selected, category) => {
+        setSelectedCategory(selected)
+        setCategory(category)
+    }
 
     return (
         <main className="h-screen w-screen px-6 pt-8 pb-20  tablet:px-28 tablet:pt-14 tablet:pb-56 desktop:px-96 desktop:pt-23 desktop:pb-48 overflow-scroll">
@@ -69,18 +80,49 @@ export default function NewFeedback(){
 
                     </div>
 
-                    <div>
+                    <div className="w-full">
                         <h1 className="leading-20 tracking-close font-bold text-13x tablet:text-14x text-xSlate-600">Category</h1>
                         <label className="text-13x tablet:text-14x font-normal text-xSlate-500">Choose a category for your feedback</label>
-                        <div className="h-12 mt-4">
-                            <select {...register("category", {required: true})}  className="mt-4 rounded-md bg-xSlate-50 px-6 py-3 text-13x tablet:text-15x text-xSlate-600 divide-x outline-none block w-full cursor-pointer focus:ring-xIndigo-600 focus:border focus:border-xIndigo-600">
-                                <option value="feature">Feature</option>
-                                <option value="ui">UI</option>
-                                <option value="ux">UX</option>
-                                <option value="bug">Bug</option>
-                                <option value="enhancement">Enhancement</option>
-                            </select>
-                        </div>
+                        
+}
+                        <button type='button' id='categoryToggle' value={Category}  data-dropdown-toggle='dropdown' className="w-full mt-4 px-6 py-3 flex bg-xSiolet-50 items-center justify-between rounded-md">
+                            <span className="font-normal text-15x text-xSlate-600 ">{SelectedCategory}</span>
+                            <svg width="10" height="7" xmlns="http://www.w3.org/2000/svg"><path d="M1 6l4-4 4 4" stroke="#4661E6" strokeWidth="2" fill="none" fillRule="evenodd"/></svg>
+                        </button>
+                        <ul id='dropdown' aria-labelledby="categoryToggle" className="z-10 hidden text-16x bg-white shadow-xl text-xSlate-500 rounded-lg divide-y-2">
+                            <li onClick={() => useCategory('Feature','feature')} className="flex px-6 py-3 items-center hover:text-xFuchisia-600 cursor-pointer justify-between">
+                                <span>Feature</span>
+                                <span className="pl-60">
+                                    {Category === 'feature' && <svg xmlns="http://www.w3.org/2000/svg" width="13" height="11"><path fill="none" stroke="#AD1FEA" strokeWidth="2" d="M1 5.233L4.522 9 12 1"/></svg> }                                  
+                                </span>
+                            </li>
+
+                            <li onClick={() => useCategory('UI','ui')} className="flex px-6 py-3 items-center hover:text-xFuchisia-600 cursor-pointer justify-between">
+                                <span>UI</span>
+                                <span className="pl-60">
+                                    {Category === 'ui' && <svg xmlns="http://www.w3.org/2000/svg" width="13" height="11"><path fill="none" stroke="#AD1FEA" strokeWidth="2" d="M1 5.233L4.522 9 12 1"/></svg> }                                  
+                                </span>
+                            </li>
+                            <li onClick={() => useCategory('UX','ux')} className="flex px-6 py-3 items-center hover:text-xFuchisia-600 cursor-pointer justify-between">
+                                <span>UX</span>
+                                <span className="pl-60">
+                                    {Category === 'ux' && <svg xmlns="http://www.w3.org/2000/svg" width="13" height="11"><path fill="none" stroke="#AD1FEA" strokeWidth="2" d="M1 5.233L4.522 9 12 1"/></svg> }                                  
+                                </span>
+                            </li>
+                            <li onClick={() => useCategory('Enhancement','enhancement')} className="flex px-6 py-3 items-center hover:text-xFuchisia-600 cursor-pointer justify-between">
+                                <span>Enhancement</span>
+                                <span className="pl-60">
+                                    {Category === 'enhancement' && <svg xmlns="http://www.w3.org/2000/svg" width="13" height="11"><path fill="none" stroke="#AD1FEA" strokeWidth="2" d="M1 5.233L4.522 9 12 1"/></svg> }                                  
+                                </span>
+                            </li>
+                            <li onClick={() => useCategory('Bug','bug')} className="flex px-6 py-3 items-center hover:text-xFuchisia-600 cursor-pointer justify-between">
+                                <span>Bug</span>
+                                <span className="pl-60">
+                                    {Category === 'bug' && <svg xmlns="http://www.w3.org/2000/svg" width="13" height="11"><path fill="none" stroke="#AD1FEA" strokeWidth="2" d="M1 5.233L4.522 9 12 1"/></svg> }                                  
+                                </span>
+                            </li>
+                        </ul>
+                        
                     </div>
                     <div>
                         <h1 className="leading-20 tracking-close font-bold text-13x tablet:text-14x text-xSlate-600">Feedback description</h1>
@@ -100,7 +142,7 @@ export default function NewFeedback(){
                             <span className="px-6 py-3 leading-20 tracking-close font-bold text-13x tablet:text-14x text-xSiolet-50">Cancel</span>
                         </button>
                         <button type="submit" className='flex bg-xFuchisia-600 hover:bg-fuchsia-500 rounded-xl mt-4 justify-center'>
-                            <span className="px-6 py-3 leading-20 tracking-close font-bold text-13x tablet:text-14x text-xSiolet-50 text-center">Add Comment</span>
+                            <span className="px-6 py-3 leading-20 tracking-close font-bold text-13x tablet:text-14x text-xSiolet-50 text-center">Add Feedback</span>
                         </button>
                     </div>
 

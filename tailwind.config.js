7272198934b35145cc6113c8e046df1113f5bf3c
defscarr <anon.aryan.admin@protonmail.com>
2023-07-25 02:32:48 +0000

    refactor: remove flowbite configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,6 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
-    "./node_modules/flowbite/**/*.js",
-    "./node_modules/flowbite-react/**/*.js",
 
 
   ],
@@ -66,6 +64,5 @@ module.exports = {
     },
   },
   plugins: [
-    require('flowbite/plugin')
   ],
 }
